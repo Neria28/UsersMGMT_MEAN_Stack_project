fce2a77cc8e24f19acfba37c23d64a755aeb7ce2
neria28 <xneriax5@gmail.com>
2021-08-10 22:23:59 +0300

    DB connection string change

--- Server/config/database.js
+++ Server/config/database.js
@@ -1,3 +1,4 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://neria29:k2jN8mSKxKCcBFyj@cluster0.tt2bx.mongodb.net/userDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://users:.@cluster0.tt2bx.mongodb.net/userDB?retryWrites=true&w=majority', 
+{ useNewUrlParser: true, useUnifiedTopology: true });
